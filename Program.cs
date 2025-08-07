2da543f252a1aca601f4371fd9c97582b1ab5f86
troy0915 <johntroydolom7@gmail.com>
2025-08-07 15:08:10 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_2Shopping_Cart_Price_Summary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> prices = new List<decimal>();
+            decimal inputPrice;
+
+            Console.WriteLine("Enter item prices (enter -1 to finish):");
+            while (true)
+            {
+                Console.Write("Enter price: ");
+                string input = Console.ReadLine();
+                if (!decimal.TryParse(input, out inputPrice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid decimal number.");
+                    continue;
+                }
+                if (inputPrice == -1)
+                {
+                    break;
+                }
+                if (inputPrice < 0)
+                {
+                    Console.WriteLine("Negative prices are not allowed (except -1 to exit).");
+                    continue;
+                }
+                prices.Add(inputPrice);
+            }
+            if (prices.Count > 0)
+            {
+                Console.WriteLine("\nResults:");
+                Console.WriteLine($"Total items: {prices.Count}");
+                Console.WriteLine($"Average price: {prices.Average():C2}");
+                Console.WriteLine($"Highest price: {prices.Max():C2}");
+                Console.WriteLine($"Lowest price: {prices.Min():C2}");
+            }
+            else
+            {
+                Console.WriteLine("\nNo prices were entered.");
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Activity 2Shopping Cart Price Summary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Activity 2Shopping Cart Price Summary")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12674f5c-f5f3-4e39-b2f3-0f204887f4ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
